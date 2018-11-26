20f967bb2b2a849490210536e8a88202c0e2714f
sebastian-soczka <sebastian.soczka@outlook.com>
2018-11-26 15:42:05 +0100

    zmieniono static root

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/rawendil/rawendil.pythonanywhere.com/mysite/blog/static'
+STATIC_ROOT = '/home/rawendil/rawendil.pythonanywhere.com/mysite/static'
